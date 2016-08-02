f414adcec7140532d8507c3e28366520b65f90fe
RThomasHyde <rthomashyde@live.com>
2016-08-02 16:03:13 -0700

    added jsPlumb

--- DecisionTree.WebAPI/App_Start/BundleConfig.cs
+++ DecisionTree.WebAPI/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace DecisionTree.WebAPI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jsplumb").Include(
+                "~/Scripts/jsplumb-{version}.js"));
         }
     }
 }
